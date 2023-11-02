8c83a3809b0875f173277211946c0da616df2537
daaniyaal <daaniyaalmirza@gmail.com>
2023-11-02 14:46:45 -0700

    Add find author and styling

--- quotes-frontend/src/components/Quote/Quote.js
+++ quotes-frontend/src/components/Quote/Quote.js
@@ -4,13 +4,30 @@ import "./Quote.scss";
 
 function Quote() {
   const [quote, setQuote] = useState(null);
+  const [authorName, setAuthorName] = useState(""); // Add this line
   useEffect(() => {
     void getQuote();
   }, []);
 
+  const handleAuthorChange = (event) => {
+    setAuthorName(event.target.value);
+  };
+
   const getQuote = async () => {
     try {
       const data = await axios.get("http://localhost:8080");
+      setQuote(data.data);
+    } catch (e) {
+      setQuote({ quote: "Couldn't find a quote." });
+    }
+  };
+
+  //Find specific quote function
+
+  const findSpecificQuote = async () => {
+    try {
+      const data = await axios.get(`http://localhost:8080/${authorName}`);
+      console.log(data);
       setQuote(data.data);
     } catch (e) {
       setQuote({ quote: "Couldn't find a quote." });
@@ -27,13 +44,27 @@ function Quote() {
       ) : (
         <div className="quote__loading">Finding a new quote...</div>
       )}
-      <button
-        disabled={!quote}
-        className="quote__button"
-        onClick={() => void getQuote()}
-      >
-        New Quote
-      </button>
+      <div className="quote__container">
+        <button
+          disabled={!quote}
+          className="quote__button"
+          onClick={() => void getQuote()}
+        >
+          New Quote
+        </button>
+        <div className="quote_bottom">
+          <input
+            className="authorName"
+            value={authorName}
+            onChange={handleAuthorChange}
+            name="authorName"
+            placeholder="Search for Author"
+          ></input>
+          <button onClick={findSpecificQuote} className="quote__button">
+            Find
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
