732db3f8bbddd187f3b71f7415e05819d287bbfc
Taylor Maxwell <taylor.w.maxwell@gmail.com>
2023-11-02 11:17:11 -0700

    Add post quote and lookup by author routes

--- quotes-api/server.js
+++ quotes-api/server.js
@@ -13,9 +13,36 @@ app.use(cors());
 
 //Getting quotes
 
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   const quotes = JSON.parse(fs.readFileSync("./data/quotes.json"));
   res.send(quotes[Math.floor(Math.random() * quotes.length)]);
+});
+
+app.post("/", (req, res) => {
+  const quotes = JSON.parse(fs.readFileSync("./data/quotes.json"));
+
+  const newQuote = {
+    q: req.body.quote,
+    a: req.body.author,
+    c: "randomnumebr",
+    h: "randomblockquote",
+  };
+
+  quotes.push(newQuote);
+  fs.writeFileSync("./data/quotes.json", JSON.stringify(quotes));
+  res.send("made it to server");
+});
+
+//get quotes by author
+
+app.get("/:author", (req, res) => {
+  const quotes = JSON.parse(fs.readFileSync("./data/quotes.json"));
+  const foundQuote = quotes.find((quote) => quote.a === req.params.author);
+  if (foundQuote) {
+    res.send(foundQuote);
+  } else {
+    res.status(400).send("quote not found");
+  }
 });
 
 app.listen(PORT, () => {
